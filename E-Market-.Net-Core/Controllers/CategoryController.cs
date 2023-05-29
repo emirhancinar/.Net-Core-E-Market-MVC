7ff8e95d3deb18c2b0bb14568d9a6a7a0affada8
Emirhan Çınar <emirhancinar025@gmail.com>
2023-05-29 22:42:25 +0300

    CategoryController created

---
+++ E-Market-.Net-Core/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Market_.Net_Core.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
