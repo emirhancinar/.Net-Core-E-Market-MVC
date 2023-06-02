207b86c5bf7faca706defc607a3333b00ec10f1c
Emirhan Çınar <emirhancinar025@gmail.com>
2023-06-02 23:20:23 +0300

    Pulling Categories from the database

--- E-Market-.Net-Core/Controllers/CategoryController.cs
+++ E-Market-.Net-Core/Controllers/CategoryController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using E_Market_.Net_Core.Models.DB_Classes;
 
 namespace E_Market_.Net_Core.Controllers
 {
     public class CategoryController : Controller
     {
+        MarketContext marketContext = new MarketContext();
         public IActionResult Index()
         {
-            return View();
+            var values=marketContext.Categories.ToList();
+            return View(values);
         }
     }
 }
